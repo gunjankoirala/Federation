83855405cf398f29811916471d3c343e8f3dd683
Gunjan koirala <gunjankoirala60@gmail.com>
2025-08-15 08:29:15 +0545

    Initial empty commit for main branch

--- users/migrations/0000_workable_ben_parker.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-	`id` varchar(255) NOT NULL,
-	`email` varchar(255),
-	`password` varchar(255) NOT NULL,
-	CONSTRAINT `user_id` PRIMARY KEY(`id`),
-	CONSTRAINT `user_email_unique` UNIQUE(`email`)
-);
--- users/src/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { mysqlTable, varchar } from "drizzle-orm/mysql-core";
-
-export const usersTable = mysqlTable('user', {
-  id: varchar({ length: 255 }).primaryKey(),
-  email: varchar({ length: 255 }).unique(),
-  password: varchar({ length: 255 }).notNull(),
-});
